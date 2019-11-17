ba35a7521ce9c87ca5bc17c4d6d803ef58e435a7
jmsether <mariowaffer@gmail.com>
2019-11-16 22:57:17 -0700

    Added comments to the RandomGenerator class

--- src/com/sethy/RandomGenerator.java
+++ src/com/sethy/RandomGenerator.java
@@ -9,11 +9,14 @@ import java.util.Random;
 
 public class RandomGenerator {
 
+    //These can be changed or added to from the files in the names folder.
+    //DO NOT RENAME FILES
     private ArrayList<String> fNames = new ArrayList<String>();
     private ArrayList<String> lNames = new ArrayList<String>();
     private ArrayList<String> places = new ArrayList<String>();
     private ArrayList<String> states = new ArrayList<String>();
 
+    //This can be added to or changed without breaking anything.
     private String[] streetType = {
             "Road",
             "Street",
@@ -31,10 +34,11 @@ public class RandomGenerator {
             "Vale"
     };
 
+    //Used heavily in this class to generate random ints
     private Random r =  new Random();
 
 
-
+    //Loads an arraylist from a giving file on disk
     private void loadArrayList(ArrayList list, Scanner file){
         while(file.hasNextLine()){
             String x = file.nextLine();
@@ -80,10 +84,13 @@ public class RandomGenerator {
          */
     }
 
+    //Returns a probable street name from file.
     private String streetT(){
         return streetType[r.nextInt(streetType.length)];
     }
 
+    //Decides if an address is an apartment or not
+    //returns apartment number if address should be an apartment
     private String apt(){
         if(r.nextInt(4) == 0){
             return " #" + r.nextInt(999);
@@ -92,6 +99,8 @@ public class RandomGenerator {
         }
     }
 
+    //returns a randomly generated house and street name from file
+    //Can randomly generate whether an address is an apartment
     public String getRAddress(){
         return  "" +
                 r.nextInt(9999) + " " +
@@ -101,14 +110,17 @@ public class RandomGenerator {
 
     }
 
+    //returns a random (Probable) City name from file.
     public String getCity(){
         return places.get(r.nextInt(places.size()));
     }
 
+    //returns a random state
     public String getState(){
         return states.get(r.nextInt(states.size()));
     }
 
+    //Returns a random zipcode
     public String getZipCode(){
         int x = r.nextInt(9);
         for(int i = 0; i < 4;i++){
@@ -117,6 +129,7 @@ public class RandomGenerator {
         return String.valueOf(x);
     }
 
+    //returns a random generated number formatted
     public String getPhoneNum(){
         return  "("+
                 getFRNum(3)+
@@ -126,6 +139,7 @@ public class RandomGenerator {
                 getFRNum(4);
     }
 
+    //Generates a number with a fixed length
     public String getFRNum(int limit){
         int x = 1+r.nextInt(9);
         for(int i = 0; i < limit-1;i++){
@@ -134,6 +148,7 @@ public class RandomGenerator {
         return String.valueOf(x);
     }
 
+    //Generates a number with desired length
     public String getRNum(int limit){
         int x = 9;
         for(int i = 0; i < limit; i++){
@@ -143,6 +158,7 @@ public class RandomGenerator {
         return String.valueOf(r.nextInt(x));
     }
 
+    //Returns ether a random first or last name from file.
     public String getRName(Boolean sirName){
         if(sirName){
             return lNames.get(r.nextInt(lNames.size()));
